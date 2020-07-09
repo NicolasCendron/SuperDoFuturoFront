23313f59a8a7e2ed4026bee00cce6c4d15bddfbf
Nicolas Cendron Fernandes <nicolas_cendron@hotmail.com>
2020-07-09 17:27:03 -0300

    Prepara a 2-way comuniccation com o backend

--- client/client.js
+++ client/client.js
@@ -1,144 +1,164 @@
-const optionsLocale = {style: 'currency', currency: 'BRL'};
+const optionsLocale = { style: "currency", currency: "BRL" };
 
-var socket = io.connect("http://127.0.0.1:3000",{secure: true});
+var socket = io.connect("http://127.0.0.1:3000", { secure: true });
 
-function pegaQuantidadeTr($tr){
-    const $tdsAtuais = $($tr.find('td'))
-    return parseInt($($tdsAtuais.get(2)).html(),10)
+function pegaQuantidadeTr($tr) {
+  const $tdsAtuais = $($tr.find("td"));
+  return parseInt($($tdsAtuais.get(2)).html(), 10);
 }
-function pegaValorTr($tr){
-    const $tdsAtuais = $($tr.find('td'))
-    const valor = parseFloat($($tdsAtuais.get(3)).html().replace(",",".").split(";")[1])
-    return valor;
+function pegaValorTr($tr) {
+  const $tdsAtuais = $($tr.find("td"));
+  const valor = parseFloat(
+    $($tdsAtuais.get(3)).html().replace(",", ".").split(";")[1]
+  );
+  return valor;
 }
-function calculaTotalCarrinho(){
-    let valorTotal = 0;
-    let qtdeTotal = 0;
-    let valorAtual = 0;
-    let qtdeAtual = 0;
-    const $rowsLista = $('#tableLista > tbody > tr')
-    $rowsLista.each((k,tr) => {
-        qtdeAtual = pegaQuantidadeTr($(tr));
-        qtdeTotal += qtdeAtual;
-        valorAtual = pegaValorTr($(tr));
-        valorTotal += qtdeAtual * valorAtual
-        console.log(qtdeAtual)
-    });
+function calculaTotalCarrinho() {
+  let valorTotal = 0;
+  let qtdeTotal = 0;
+  let valorAtual = 0;
+  let qtdeAtual = 0;
+  const $rowsLista = $("#tableLista > tbody > tr");
+  $rowsLista.each((k, tr) => {
+    qtdeAtual = pegaQuantidadeTr($(tr));
+    qtdeTotal += qtdeAtual;
+    valorAtual = pegaValorTr($(tr));
+    valorTotal += qtdeAtual * valorAtual;
+    console.log(qtdeAtual);
+  });
 
-    $('#qtdeTotalCompra').html(qtdeTotal);
-    $('#valorTotalCompra').html(valorTotal.toLocaleString('pt-BR', optionsLocale));
-
+  $("#qtdeTotalCompra").html(qtdeTotal);
+  $("#valorTotalCompra").html(
+    valorTotal.toLocaleString("pt-BR", optionsLocale)
+  );
 }
-function calculaTotalEstoque(){
-    let valorTotal = 0;
-    let qtdeTotal = 0;
-    let valorAtual = 0;
-    let qtdeAtual = 0;
-    const $rowsLista = $('#tableEstoque > tbody > tr')
-    $rowsLista.each((k,tr) => {
-        qtdeAtual = pegaQuantidadeTr($(tr));
-        qtdeTotal += qtdeAtual;
-        valorAtual = pegaValorTr($(tr));
-        valorTotal += qtdeAtual * valorAtual
-    });
+function calculaTotalEstoque() {
+  let valorTotal = 0;
+  let qtdeTotal = 0;
+  let valorAtual = 0;
+  let qtdeAtual = 0;
+  const $rowsLista = $("#tableEstoque > tbody > tr");
+  $rowsLista.each((k, tr) => {
+    qtdeAtual = pegaQuantidadeTr($(tr));
+    qtdeTotal += qtdeAtual;
+    valorAtual = pegaValorTr($(tr));
+    valorTotal += qtdeAtual * valorAtual;
+  });
 
-    $('#qtdeTotalEstoque').html(qtdeTotal);
-    $('#valorTotalEstoque').html(valorTotal.toLocaleString('pt-BR', optionsLocale));
+  $("#qtdeTotalEstoque").html(qtdeTotal);
+  $("#valorTotalEstoque").html(
+    valorTotal.toLocaleString("pt-BR", optionsLocale)
+  );
 }
 
 function populaListaDeCompras(json) {
-    const oLista = json.produtos;
-    for (let i = 0; i < oLista.length; i++) {
-        addLine($('#tableLista'), oLista[i]);
-    }
-    calculaTotalCarrinho();
+  limpaTable($("#tableLista > tbody > tr"));
+  const oLista = json.produtos;
+  for (let i = 0; i < oLista.length; i++) {
+    oLista[i].quantidade = "0";
+    addLine($("#tableLista"), oLista[i]);
+  }
+  calculaTotalCarrinho();
 }
 
 function populaEstoque(json) {
-    const oLista = json.produtos;
-    for (let i = 0; i < oLista.length; i++) {
-        const qtde = parseInt(oLista[i].quantidade);
-        addLine($('#tableEstoque'), oLista[i]);
-    }
-    calculaTotalEstoque();
-
+  limpaTable($("#tableEstoque > tbody > tr"));
+  const oLista = json.produtos;
+  for (let i = 0; i < oLista.length; i++) {
+    const qtde = parseInt(oLista[i].quantidade);
+    addLine($("#tableEstoque"), oLista[i]);
+  }
+  calculaTotalEstoque();
 }
 
-function incrementaItem(rowLista){
-    const arrTD = $(rowLista).find('td')
-    const td = $(arrTD)[2]
-    const valor = parseInt($(td).html(),10)
-    $(td).html(valor + 1)
-
-    $(rowLista).addClass('bg-success');
-    setTimeout(function() {
-        $(rowLista).removeClass('bg-success');
-    }, 1000);
-
+function limpaTable($listTrs) {
+  $listTrs.remove();
 }
-function decrementaItem(rowLista){
-    const arrTD = $(rowLista).find('td')
-    const td = $(arrTD)[2]
-    const valor = parseInt($(td).html(),10)
-    $(td).html(valor - 1)
 
-    $(rowLista).addClass('bg-danger');
-    setTimeout(function() {
-        $(rowLista).removeClass('bg-danger');
-    }, 1000);
+function incrementaItem(rowLista) {
+  const arrTD = $(rowLista).find("td");
+  const td = $(arrTD)[2];
+  const valor = parseInt($(td).html(), 10);
+  $(td).html(valor + 1);
+
+  $(rowLista).addClass("bg-success");
+  setTimeout(function () {
+    $(rowLista).removeClass("bg-success");
+  }, 1000);
+}
+function decrementaItem(rowLista) {
+  const arrTD = $(rowLista).find("td");
+  const td = $(arrTD)[2];
+  const valor = parseInt($(td).html(), 10);
+  $(td).html(valor - 1);
+
+  $(rowLista).addClass("bg-danger");
+  setTimeout(function () {
+    $(rowLista).removeClass("bg-danger");
+  }, 1000);
 }
 function addLine(table, p) {
-    const html = '<tr>' +
-        '<td>' + p.id + '</td>' +
-        '<td>' + p.nome + '</td>' +
-        '<td class="text-right">' + p.quantidade + '</td>' +
-        '<td class="text-right">' + parseFloat(p.preco).toLocaleString('pt-BR', optionsLocale) + '</td></tr>';
-    table.find('tbody').append(html);
+  const html =
+    "<tr>" +
+    "<td>" +
+    p.id +
+    "</td>" +
+    "<td>" +
+    p.nome +
+    "</td>" +
+    '<td class="text-right">' +
+    p.quantidade +
+    "</td>" +
+    '<td class="text-right">' +
+    parseFloat(p.preco).toLocaleString("pt-BR", optionsLocale) +
+    "</td></tr>";
+  table.find("tbody").append(html);
 }
 
 $(document).ready(function () {
-    var jsonLista = {
-        'produtos': [
-            {'id': '1', 'nome': 'Ades', 'quantidade': '0', 'preco': '4.50'},
-            {'id': '2', 'nome': 'Visconti', 'quantidade': '0', 'preco': '3.00'},
-            {'id': '3', 'nome': 'Italac', 'quantidade': '0', 'preco': '4.50'},
-        ]
-    }
+  var jsonEstoque = {
+    produtos: [
+      { id: "1", nome: "Ades", quantidade: "49", preco: "4.50" },
+      { id: "2", nome: "Visconti", quantidade: "23", preco: "3.00" },
+      { id: "3", nome: "Italac", quantidade: "34", preco: "4.50" },
+    ],
+  };
+  populaEstoque(jsonEstoque);
+  populaListaDeCompras(jsonEstoque);
+  console.log("Start");
+  socket.emit("start");
 
-    var jsonEstoque = {
-        'produtos': [
-            {'id': '1', 'nome': 'Ades', 'quantidade': '49', 'preco': '4.50'},
-            {'id': '2', 'nome': 'Visconti', 'quantidade': '23', 'preco': '3.00'},
-            {'id': '3', 'nome': 'Italac', 'quantidade': '34', 'preco': '4.50'},
-        ]
-    }
+  socket.on("add", function (id) {
+    const rowLista = $("#tableLista > tbody > tr")[parseInt(id, 10) - 1];
+    const rowEstoque = $("#tableEstoque > tbody > tr")[parseInt(id, 10) - 1];
 
-    populaListaDeCompras(jsonLista);
-    populaEstoque(jsonEstoque);
+    incrementaItem(rowLista);
+    decrementaItem(rowEstoque);
+    calculaTotalCarrinho();
+    calculaTotalEstoque();
+    console.log("Adicionou " + id);
+  });
 
-    socket.on("add", function(id) {
-        const rowLista = $('#tableLista > tbody > tr')[parseInt(id,10) - 1];
-        const rowEstoque =  $('#tableEstoque > tbody > tr')[parseInt(id,10) - 1]
+  socket.on("remove", function (id) {
+    const rowLista = $("#tableLista > tbody > tr")[parseInt(id, 10) - 1];
+    const rowEstoque = $("#tableEstoque > tbody > tr")[parseInt(id, 10) - 1];
 
-        incrementaItem(rowLista);
-        decrementaItem(rowEstoque);
-        calculaTotalCarrinho();
-        calculaTotalEstoque();
-        console.log("Adicionou " + id)
-        
-    });
+    incrementaItem(rowEstoque);
+    decrementaItem(rowLista);
 
-    socket.on("remove", function(id) {
-        const rowLista = $('#tableLista > tbody > tr')[parseInt(id,10) - 1];
-        const rowEstoque =  $('#tableEstoque > tbody > tr')[parseInt(id,10) - 1]
+    calculaTotalEstoque();
+    calculaTotalCarrinho();
+    console.log("Removeu" + id);
+  });
 
-        incrementaItem(rowEstoque);
-        decrementaItem(rowLista);
+  socket.on("populateStock", function (jsonListaCompras) {
+    console.log(jsonListaCompras);
+    populaEstoque(jsonListaCompras);
+    populaListaDeCompras(jsonListaCompras);
+    console.log("Populou Estoque");
+  });
 
-        calculaTotalEstoque();
-        calculaTotalCarrinho();
-        console.log("Removeu" + id)
-    });
-
+  socket.on("startSuccess", function (jsonListaCompras) {
+    console.log("Autorizado Login");
+  });
 });
-
--- server/server.js
+++ server/server.js
@@ -1,21 +1,39 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const app = express();
-
-
+app.use(express.json());
 const server = http.Server(app);
 const io = socketIO(server);
+console.log("Server Running on localhost:3000");
+console.log("API:");
+console.log("Add (GET), Remove (GET), PopulateStock (POST)");
 
-app.get('/Add', (req, res) => {
-    io.emit("add",req.query.id)
-    res.sendStatus(200);
-})
-app.get('/Remove', (req, res) => {
-    io.emit("remove",req.query.id)
-    res.sendStatus(200);
-})
+//Escuta o Backend e manda ordens pra o Client
+
+app.post("/Add", (req, res) => {
+  io.emit("add", req.query.id);
+  console.log("Adicionou Produto com ID: " + req.query.id);
+  res.sendStatus(200);
+});
+app.post("/Remove", (req, res) => {
+  io.emit("remove", req.query.id);
+  console.log("Removeu Produto com ID:" + req.query.id);
+  res.sendStatus(200);
+});
+app.post("/PopulateStock", (req, res) => {
+  io.emit("populateStock", req.body);
+  console.log(req.body);
+  console.log("Populou Estoque");
+  res.sendStatus(200);
+});
 server.listen(3000);
 
+//Inicializa o Backend quando o cliente faz Login
 
-
+io.sockets.on("connection", function (socket) {
+  socket.on("start", function (data) {
+    console.log("Enviado Start para o Backend");
+    io.emit("startSuccess");
+  });
+});
